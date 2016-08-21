2eff47d56df3743cfc14050e1c1c6307147d6f0a
y_okada <yasokada@gmail.com>
2016-08-21 11:00:05 +0900

    update UpdateInfo()

--- Assets/InventoryCS.cs
+++ Assets/InventoryCS.cs
@@ -5,6 +5,7 @@ using NS_SampleData;
 using NS_MyStringUtil;
 
 /*
+ * v0.2 2016 Aug. 21
  *   - add UpdateInfo()
  *   - add [MyStringUtil.cs]
  *   - add OpenURL()
@@ -25,10 +26,13 @@ public class InventoryCS : MonoBehaviour {
 	public Text T_about;
 	public Text T_datasheetURL;
 
+	// TOOD: 0m > put other place to declear
 	const int kIndex_caseNo = 0;
 	const int kIndex_row = 1;
 	const int kIndex_column = 2;
-	const int kIndex_dataSheetURL = 5; // TODO: put other place to declare
+	const int kIndex_name = 3;
+	const int kIndex_about = 4;
+	const int kIndex_dataSheetURL = 5;
 
 	void Start () {
 		T_about.text = NS_SampleData.SampleData.GetDataOfRow (0);	
@@ -42,7 +46,8 @@ public class InventoryCS : MonoBehaviour {
 		T_caseNo.text = MyStringUtil.ExtractCsvColumn (datstr, kIndex_caseNo);
 		T_row.text = MyStringUtil.ExtractCsvColumn (datstr, kIndex_row);
 		T_column.text = MyStringUtil.ExtractCsvColumn (datstr, kIndex_column);
-		T_about.text = datstr;
+		IF_name.text = MyStringUtil.ExtractCsvColumn (datstr, kIndex_name);
+		T_about.text = MyStringUtil.ExtractCsvColumn (datstr, kIndex_about);
 		T_datasheetURL.text = MyStringUtil.ExtractCsvColumn (datstr, kIndex_dataSheetURL);
 	}
 
