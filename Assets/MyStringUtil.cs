110b2633f75f258b47d13564a5637de08dccf284
y_okada <yasokada@gmail.com>
2016-08-27 07:23:42 +0900

    MyStringUtil: handle csv with cells having comma inside the item

--- Assets/MyStringUtil.cs
+++ Assets/MyStringUtil.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Linq;
 using System; // for StringSplitOptions.RemoveEmptyEntries
+using System.Collections.Generic;
+using System.Text.RegularExpressions; // for Regex
 
 /*
+ * v0.4 2016 Aug. 27
+ *   - handle csv with cells having comma inside the item
+ *     + update ExtractCsvColumn() to use SplitCSV()
+ *     + add SplitCSV()
  * v0.3 2016 Aug. 21
  *   - add ExtractCsvColumn()
  * v0.2 2015/11/21
@@ -51,8 +57,25 @@ namespace NS_MyStringUtil
 			return work;
 		}
 
+		public static string[] SplitCSV(string input)
+		{
+			Regex csvSplit = new Regex ("(?:^|,)(\"(?:[^\"]+|\"\")*\"|[^,]*)", RegexOptions.Compiled);
+			List<string> list = new List<string> ();
+			string curr = null;
+			foreach (Match match in csvSplit.Matches(input)) {        
+				curr = match.Value;
+				if (0 == curr.Length) {
+					list.Add ("");
+				}
+
+				list.Add (curr.TrimStart (','));
+			}
+
+			return list.ToArray<string> ();
+		}
+
 		public static string ExtractCsvColumn(string src, int idx) {
-			string [] elements = src.Split(',');
+			string [] elements = SplitCSV(src);
 			if (elements.Length <= idx) {
 				return "";
 			}
