4c9dfac2b52150a7177ca9e53fbda1134e18f026
y_okada <yasokada@gmail.com>
2016-08-27 08:00:25 +0900

    add [ScreenSizeKeeper.cs]

---
+++ Assets/ScreenSizeKeeper.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/*
+ * v0.3 2016 Aug. 27
+ *   - change [buttonRatio]
+ * v0.2 2015/11/21
+ *   - change myStandard from [Button] to [Text]
+*/
+
+public class ScreenSizeKeeper : MonoBehaviour {
+
+	public Text myStandard; // set component whose width is used as standard
+
+	bool isRunningOnAndroid() {
+		return (Application.platform == RuntimePlatform.Android);
+	}
+
+	void Start () {
+		if (isRunningOnAndroid () == false) {
+			return;
+		}
+		float aspect = (float)Screen.height / (float)Screen.width;
+		float buttonRatio = 0.35f; // 35%
+		int buttonWidth = (int)myStandard.GetComponent<RectTransform> ().rect.width;
+		float newWidth, newHeight;
+
+		newWidth = buttonWidth / buttonRatio;
+		newHeight = newWidth * aspect;
+
+		Screen.SetResolution ((int)newWidth, (int)newHeight, false);
+	}   
+
+}
